78e25cd79b6ff203dfef493d85fc5fca524d6632
Julianne Stensen <julianne.stensen@gmail.com>
2014-08-04 00:47:47 +0200

    Lagt til html for kursregistrering, ikke ferdig

--- angular-seed/app/js/controllers.js
+++ angular-seed/app/js/controllers.js
@@ -3,7 +3,11 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('Registrer', ['$scope', function($scope) {
+
+
+
+  	
 
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {

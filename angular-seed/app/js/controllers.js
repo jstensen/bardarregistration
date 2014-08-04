5e869e2afe1d1cc8100ccaf27580f78b71961359
Julianne Stensen <julianne.stensen@gmail.com>
2014-08-05 00:35:53 +0200

    filer

--- angular-seed/app/js/controllers.js
+++ angular-seed/app/js/controllers.js
@@ -3,9 +3,22 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('Registrer', ['$scope', function($scope) {
+  .controller('Registrer', ['$scope', 'TestService', function($scope, TestService) {
 
+$scope.test = "TesterString";
 
+$scope.participant = {}; 
+
+$scope.registrerParticipant = function(participant) {
+	console.log(participant.name);
+	console.log(participant.address);
+	console.log(participant.email);
+	console.log(participant.phonenumber);
+	console.log(participant.gender);
+	console.log(participant.dateofbirth);
+
+	TestService.registrerParticipant();
+};
 
   	
 
--- angular-seed/app/js/services.js
+++ angular-seed/app/js/services.js
@@ -6,4 +6,24 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
+	service('TestService', function($http) {
+		//something here
+		this.registrerParticipant = function(participant) {
+
+			var test = {name: "4",
+						description: "Testkurs fra Julianne",
+						capacity: 6, 
+						maxUnbalance: 2, 
+						status: "Open"};
+
+			$http.post("http://192.168.2.3/bardarregistration/addcourse.php", test);
+
+
+			console.log("Hepp");
+		};
+
+
+
+	}).
+	
   value('version', '0.1');
